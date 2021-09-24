0782175c20f1dcbe7b7b2116735e04ca640d85e4
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-09-24 22:50:09 +1000

    configurate webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const webpack = require('webpack')
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin") //
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
@@ -21,12 +23,16 @@ module.exports = {
         ],
 
         resolve: {
-            modules: [
-                dirApp,
-                dirShared,
-                dirStyles,
-                dirNode
-            ]
+            modules: [  //
+                "dirApp",
+                "dirShared",
+                "dirStyles",
+                "dirNode",
+                "src",           //OJO CON ESTO
+                "node_modules"  //OJO CON ESTO
+            ],
+
+            extensions: ['.js', '.jsx'] //
     },
 
         plugins: [
@@ -34,13 +40,101 @@ module.exports = {
                 IS_DEVELOPMENT
             }),
 
-            new CopyWebpackPlugin({
+            new CopyWebpackPlugin({  //Careful here with the webpack.CopyWebpackPlugin vs CopyWebpackPlugin situation
                 patterns: [
                     {
                         from: './shared',
                         to: '',
                     }
                 ]
-            })
-        ]
+            }),
+
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            }),
+
+            new ImageMinimizerPlugin({
+                minimizerOptions: {
+                    // Lossless optimization with custom option
+                    // Feel free to experiment with options for better result for you
+                    plugins: [
+                        ["gifsicle", { interlaced: true }],
+                        ["jpegtran", { progressive: true }],
+                        ["optipng", { optimizationLevel: 8 }]
+
+                    ],
+                },
+            }),
+
+            new NodePolyfillPlugin() //
+
+        ],
+
+    module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                publicPath: ''
+                            }
+                        },
+
+                        {
+                            loader: 'css-loader'
+                        },
+
+                        {
+                            loader: 'postcss-loader'
+                        },
+
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                },
+
+                {
+                    test: /\. (jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                    loader: 'file-loader',
+                    options: {
+                        name (file) {
+                            return '[hash].[ext]'
+                        }
+                    }
+                },
+
+                {
+                    test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                    use: [
+                        {
+                            loader: ImageMinimizerPlugin.loader,
+
+                        },
+                    ],
+                },
+
+                {
+                    test: /\.(glsl|frag|vert)$/,
+                    loader: 'raw-loader',
+                    exclude: /node_modules/
+                },
+
+                {
+                    test: /\.(glsl|frag|vert)$/,
+                    loader: 'glslify-loader',
+                    exclude: /node_modules/
+                }
+            ]
+    }
 }
